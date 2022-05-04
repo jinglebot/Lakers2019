00ebde9957b736421f588282dbda66576400a65c
MARIA CRISTINA SALES AN <maa7575@g.harvard.edu>
2022-05-04 15:18:24 -0700

    Update 2_Term Project - NBA Lakers' Stat Analysis.R
    Added numbering in the glossary, correlations, linear and logistic regressions

--- 2_Term Project - NBA Lakers' Stat Analysis.R
+++ 2_Term Project - NBA Lakers' Stat Analysis.R
@@ -1,56 +1,65 @@
 # MATH E-23C
-# Term Project Initial EDA
+# Term Project 
 
 # Submitted by: Maria Cristina An, Daniel Lebedinsky, Julio Solis Arce
-# Date: March 23, 2022
+# Date: May 6, 2022
 
 # 1. Reference: https://www.basketball-reference.com/teams/LAL/2020/gamelog/
 Lakers <- read.csv("Lakers 2019-20 Game log.csv"); head(Lakers)
 
+# Libraries
+# need to be run at most once
+# install.packages("PerformanceAnalytics") 
+library("PerformanceAnalytics") # for correlations
+#install.packages("stats4")   
+library(stats4) # for logistic regression
+
+
 # Glossary
-# Rk -- Rank
-# G -- Season Game
-# W_E -- opponent is from East or West
-# Opp -- Opponent
-# W_L -- Lakers team won or lost
-# Tm -- Lakers team Points
-# Opp_pts -- Opponent Points
+# 1 Rk -- Rank
+# 2 G -- Season Game
+# 3 Date
+# 4 W_E -- opponent is from East or West
+# 5 Opp -- Opponent
+# 6 W_L -- Lakers team won or lost
+# 7 Tm -- Lakers team Points
+# 8 Opp_pts -- Opponent Points
 
 # Lakers 
-# FG_LA -- Field Goals
-# FGA_LA -- Field Goal Attempts
-# FGpc_LA -- Field Goal Percentage
-# X3P_LA -- 3-Point Field Goals
-# X3PA_LA -- 3-Point Field Goal Attempts
-# X3Ppc_LA -- 3-Point Field Goal Percentage
-# FT_LA -- Free Throws
-# FTA_LA -- Free Throw Attempts
-# FTpc_LA -- Free Throw Percentage
-# ORB_LA -- Offensive Rebounds
-# TRB_LA -- Total Rebounds
-# AST_LA -- Assists
-# STL_LA -- Steals
-# BLK_LA -- Blocks
-# TOV_LA -- Turnovers
-# PF_LA -- Personal Fouls
+# 9 FG_LA -- Field Goals
+# 10 FGA_LA -- Field Goal Attempts
+# 11 FGpc_LA -- Field Goal Percentage
+# 12 X3P_LA -- 3-Point Field Goals
+# 13 X3PA_LA -- 3-Point Field Goal Attempts
+# 14 X3Ppc_LA -- 3-Point Field Goal Percentage
+# 15 FT_LA -- Free Throws
+# 16 FTA_LA -- Free Throw Attempts
+# 17 FTpc_LA -- Free Throw Percentage
+# 18 ORB_LA -- Offensive Rebounds
+# 19 TRB_LA -- Total Rebounds
+# 20 AST_LA -- Assists
+# 21 STL_LA -- Steals
+# 22 BLK_LA -- Blocks
+# 23 TOV_LA -- Turnovers
+# 24 PF_LA -- Personal Fouls
 
 # Opponent
-# FG -- Opponent Field Goals
-# FGA -- Opponent Field Goal Attempts
-# FGpc -- Opponent Field Goal Percentage
-# X3P -- Opponent 3-Point Field Goals
-# X3PA -- Opponent 3-Point Field Goal Attempts
-# X3Ppc -- Opponent 3-Point Field Goal Percentage
-# FT -- Opponent Free Throws
-# FTA -- Opponent Free Throw Attempts
-# FTpc -- Opponent Free Throw Percentage
-# ORB -- Opponent Offensive Rebounds
-# TRB -- Opponent Total Rebounds
-# AST -- Opponent Assists
-# STL -- Opponent Steals
-# BLK -- Opponent Blocks
-# TOV -- Opponent Turnovers
-# PF -- Opponent Personal Fouls
+# 25 FG -- Opponent Field Goals
+# 26 FGA -- Opponent Field Goal Attempts
+# 27 FGpc -- Opponent Field Goal Percentage
+# 28 X3P -- Opponent 3-Point Field Goals
+# 29 X3PA -- Opponent 3-Point Field Goal Attempts
+# 30 X3Ppc -- Opponent 3-Point Field Goal Percentage
+# 31 FT -- Opponent Free Throws
+# 32 FTA -- Opponent Free Throw Attempts
+# 33 FTpc -- Opponent Free Throw Percentage
+# 34 ORB -- Opponent Offensive Rebounds
+# 35 TRB -- Opponent Total Rebounds
+# 36 AST -- Opponent Assists
+# 37 STL -- Opponent Steals
+# 38 BLK -- Opponent Blocks
+# 39 TOV -- Opponent Turnovers
+# 40 PF -- Opponent Personal Fouls
 
 
 # Convert to proper date format
@@ -66,6 +75,7 @@ nrow(Lakers)
 # Creating categorical columns
 WonLost <- Lakers$W_L == "W"; head(WonLost) 
 sum(WonLost) # total wins
+
 
 # Graphical Displays
 
@@ -129,6 +139,7 @@ table(Less100)
 # This table shows the opposite of the table above, that despite being the all-around champion during that year, our team scored
 # less than 100 in some games and this table shows that.
 
+
 # 1. Permutation test
 # Our permutation test will be comparing our team's scores in games played 
 # vs the opponents from the East and the opponents from the West
@@ -182,10 +193,155 @@ abline(v = Observed, col = "purple")
 
 #What is the probability (the P value) that a difference this large
 #could have arisen with a random subset?
-pvalue <- (sum(Score_diffs >= Observed)+1)/(N+1); pvalue # 1
-# This goes to shows that the data observed has a very big chance 
+pvalue <- (sum(Score_diffs <= Observed)+1)/(N+1); pvalue # 1
+# This goes to shows that the data observed has a very small chance 
 # to have come about by chance. Therefore, there is insufficient evidence to
 # to reject the null hypothesis.
+
+
+# Correlations
+# We will use the Performance analytics package to show correlations.
+# p-values (0.001, 0.01, 0.05, 0.1, 1) <=> symbols("***", "**", "*", ".", "")
+
+# The correlation of Lakers' score against the the opponents score in general
+chart.Correlation(Lakers[,7:8], histogram=TRUE, pch=19)
+
+# The correlation of Lakers' score against the scores of the opponents from the West
+West <- Lakers[Western, ]; West
+chart.Correlation(West[,7:8], histogram=TRUE, pch=19)
+
+# The correlation of Lakers' score against the scores of the opponents from the East
+East <- Lakers[Eastern, ]; East
+chart.Correlation(East[,7:8], histogram=TRUE, pch=19)
+
+# The correlation of Lakers' score with their Field Goal, 3-pointers, Free throws, and Fouls
+chart.Correlation(Lakers[,c(7,9,12,15,24)], histogram=TRUE, pch=19)
+
+# The correlation of the Opponents' score with their Field Goal, 3-pointers, Free throws, and Fouls
+chart.Correlation(Lakers[,c(8,25,28,31,40)], histogram=TRUE, pch=19)
+
+
+# Multiple Linear Regression
+
+# Let us see how the Lakers performed against Western opponents
+# Let's extract only the Western games and use the columns for 3-point Shots and Field Goals
+WestPts <- Lakers$Tm[Western]; length(WestPts)
+West3P <- Lakers$X3P_LA[Western]; head(West3P)
+WestFG <- Lakers$FG_LA[Western]; head(WestFG)
+plot(WestPts,West3P)  
+plot(WestPts,WestFG) 
+West <- Lakers[Western,]; nrow(West)
+
+cor(WestPts, West3P)     
+chart.Correlation(West[,c(7,12)], histogram=TRUE, pch=19)
+cor(WestPts,WestFG)       
+chart.Correlation(West[,c(7,9)], histogram=TRUE, pch=19)
+
+# To minimize residuals, project onto the subspace spanned by constant
+# 3 pointers, and free throws
+m1 <- rep(1,length(West$X3P_LA))    #constant
+m2 <- West$X3P_LA    
+m3 <- West$FG_LA
+
+A <- cbind(m1,m2,m3)   # columns span a 3 dimensional subspace
+# Make an invertible square matrix
+B <- t(A)%*%A; B
+# Invert it
+BInv <- solve(B); BInv
+# Make the projection matrix.
+P <- A%*%BInv%*%t(A)
+
+PredictScore <- P%*%West$Tm   # the projection of the score onto subspace M
+coeff <- BInv%*%t(A)%*%West$Tm; coeff
+
+Resid <- West$Tm - coeff[1]*m1 - coeff[2]*m2 - coeff[3]*m3        # the residuals
+sum(Resid*m1); sum(Resid*m2) ; sum(Resid*m3) # orthogonal to all three basis vectors
+sqrt(sum(Resid^2))                   # this is the minimum possible length
+# Splitting up the variance
+varObs <- var(West$Tm); varObs # variance of observations
+varPred <- var(PredictScore); varPred # variance of predicted values
+varResid <-var(Resid); varResid # variance of residuals
+
+# The variance of the observations is the sum of the two pieces
+varObs; varPred +  varResid
+# Our predictors now explain a greater fraction of the variance
+varPred/varObs
+
+# Of course this has all been automated
+FG_3P <- lm(West$Tm~West$X3P_LA+West$FG_LA, data =West);FG_3P
+coeff   # we got the same coefficients
+summary(FG_3P)
+
+
+# Regression with a higher-degree polynomial
+# Let's try linear regression on multiple variables, we'll use FT, FG and 3P 
+West_stat <- West$FT_LA+2*West$FG_LA+3*West$X3P_LA; head(Score); head(West$Tm)  # stats per game
+hist(West$Tm)   
+plot(West_stat, West$Tm, pch = '.', cex = 3)
+# Let's try a linear model
+m1 <- rep(1,length(West_stat))    #constant
+m2 <- West_stat    
+
+A <- cbind(m1,m2)   #columns span a 2 dimensional subspace
+#Make an invertible square matrix
+B <- t(A)%*%A; B
+#Invert it
+BInv <- solve(B); BInv
+coeff <- BInv%*%t(A)%*%West$Tm; coeff
+f <- function(x) coeff[1]+coeff[2]*x
+curve(f, add = TRUE)    
+
+#Just include West_stat^2 as an extra predictor.
+m3 <- West_stat^2    
+A <- cbind(m1,m2,m3)   #columns span a 3 dimensional subspace
+#Make an invertible square matrix
+B <- t(A)%*%A; B
+#Invert it
+BInv <- solve(B); BInv
+coeff <- BInv%*%t(A)%*%West$Tm; coeff
+f <- function(x) coeff[1]+coeff[2]*x + coeff[3]*x^2
+curve(f, add = TRUE, col = "red")   
+
+
+# Logistic regression
+
+# Extract the scores column of the Eastern opponents and on which ones the Lakers won
+EastPts <- Lakers$Opp_pts[Eastern]; EastPts
+EastWL <- Lakers$W_L[Eastern] == "W"; head(EastWL) 
+plot(EastPts,EastWL)  # not a great candidate for a straight-line approximation, but let's try
+# Get the slope
+b <- cov(EastPts,EastWL)/var(EastPts); b   
+# Get the intercept
+a <- mean(EastWL) - b*mean(EastPts);a  
+# Add this regression line to the plot of the data
+abline(a, b, col = "red")
+# This is not a good approximation.
+
+# Get the predictor
+pred <- EastPts
+# Get the response
+resp <- EastWL
+# Let's use the function for multiple logistic regression
+MLL<- function(alpha, beta) {
+  -sum( log( exp(alpha+beta*pred)/(1+exp(alpha+beta*pred)) )*resp
+        + log(1/(1+exp(alpha+beta*pred)))*(1-resp) )
+}
+# Maximize the function of alpha and beta
+results<-mle(MLL, start = list(alpha = 0, beta = 0)) # an initial guess is required
+results@coef
+plot(EastPts,EastWL) # get a clean plot  
+curve( exp(results@coef[1]+results@coef[2]*x)/ (1+exp(results@coef[1]+results@coef[2]*x)),col = "blue", add=TRUE)
+#The curve shows a good model for the probability of winning as a function of the opponents' scores
+abline(h=0.5)
+abline(v=110)
+
+index <- which(pred == 110); head(index)   # games with Eastern opponents scoring 110 pts
+mean(EastWL[index])   # The Lakers won 100% of the time.
+index <- which(pred <= 110)    # games with Eastern opponents scoring <= 110 pts
+mean(EastWL[index])   # The Lakers won 80% of the time.
+index <- which(pred > 110)    # games with Eastern opponents scoring > 110 pts
+mean(EastWL[index])   # The Lakers only won 36.3% of the time.
+
 
 # The End
 
